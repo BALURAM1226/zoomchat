ff19d53a677fcc878a79a5530924740543dbfb70
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-08 15:19:28 +0530

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 
-const socket = io.connect('https://videomiz.herokuapp.com');
+const socket = io('https://videomiz.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
