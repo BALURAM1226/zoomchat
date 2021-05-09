ca21b7966e13c534dcfca795ec9d8570400bc9da
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-09 09:01:31 +0530

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -20,7 +20,7 @@ const ContextProvider = ({ children }) => {
   const connectionRef = useRef();
 
   useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ video: { facingMode : "user" } , audio: { sampleRate: 48000, channelCount:2, noiseSuppression: false, echoCancellation: true } })
+    navigator.mediaDevices.getUserMedia({ video: { facingMode : "user" } , audio: { sampleRate: 48000, channelCount:2, noiseSuppression: constraint, echoCancellation: true } })
       .then((currentStream) => {
 
         setStream(currentStream);
